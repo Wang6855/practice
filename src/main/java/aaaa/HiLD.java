7aa0925953990da4ed7bb5fddf3123154bd8a978
denny <635485830@qq.com>
2017-03-04 22:03:23 +0800

    aa

--- src/main/java/aaaa/HiLD.java
+++ src/main/java/aaaa/HiLD.java
@@ -7,6 +7,14 @@ public static void main(String[] args) {
 	System.out.println("ni ge bitch");
 
 	System.out.println("请写一个工具类，实现‘去掉字符串中所有的空格’，不借助trim()方法");
+
+	
 }
+	public static void trimSpace(String s){
+		 s ="sb s n ";
+		 String[] split = s.split(" ");
+		 String s1 = split[0]+split[1]+split[2];
+		 System.out.println(s1);
+	}
 }
 interface chi{}
---
+++ src/main/java/wanggouzi/LaoWang.java
@@ -0,0 +1,8 @@
+package wanggouzi;
+
+public class LaoWang {
+	
+	public static void main(String[] args) {
+		System.out.println("wo jia baobao jiao wanggouzi");
+	}
+}
