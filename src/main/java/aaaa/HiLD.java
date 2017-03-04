274914d46470c6ba625b90b4e14d5c5b0c6e4906
anforious <2418916855@qq.com>
2017-03-04 21:33:03 +0800

    Test1

--- src/main/java/aaaa/HiLD.java
+++ src/main/java/aaaa/HiLD.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("你好，死胖子");
 	System.out.println("SSH");
 	System.out.println("ni ge bitch");
+	System.out.println("请写一个工具类，实现‘去掉字符串中所有的空格’，不借助trim()方法");
 }
 }
 interface chi{}
