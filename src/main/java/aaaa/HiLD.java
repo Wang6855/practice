e1446af2a444e681c829c8088731715bee96adb4
anforious <2418916855@qq.com>
2017-03-04 21:58:44 +0800

    Test2

--- src/main/java/aaaa/HiLD.java
+++ src/main/java/aaaa/HiLD.java
@@ -7,6 +7,15 @@ public static void main(String[] args) {
 	System.out.println("ni ge bitch");
 
 	System.out.println("请写一个工具类，实现‘去掉字符串中所有的空格’，不借助trim()方法");
+	
+	System.out.println("写一个文件读取类，用来读取properties文件（读出来在控制台即可）");
+	
+	System.out.println("反射调用A类的void play(String name)方法（方法体自定义）");
+	
 }
+
+	
 }
+
+
 interface chi{}
