3ef654b44eadfa68eb0a6839b56f0f92e6bc2678
anforious <2418916855@qq.com>
2017-03-21 10:57:54 +0800

    gt

---
+++ src/main/java/aaaa/GetField.java
@@ -0,0 +1,18 @@
+package aaaa;
+
+public class GetField {
+public static void main(String[] args) {
+	Foo foo = new Foo();
+boom(55);
+	System.out.println(foo.att);
+
+}
+public static int boom(int i){
+	return   (i> 5  ?boom(i):i);
+}
+}
+
+class Foo {
+	int att = 5;
+	
+}
